545098f361b5576a5daca78b6d4c76169aeff286
Tomas-Barhon <tomas.barhon@hotmail.cz>
2026-04-13 20:31:26 +0200

    preliminary formation implementation

--- Sensors/create_line_form_coords.lua
+++ Sensors/create_line_form_coords.lua
@@ -6,24 +6,24 @@ end
 
 local SpringGetTeamUnitsCounts= Spring.GetTeamUnitsCounts
 local myTeamID = Spring.GetMyTeamID()
+local SpringGetTeamUnits = Spring.GetTeamUnits
 
 return function(
     leader_pos,
     wind_vector,
     spacing
 )
-	local unitsCounts = SpringGetTeamUnitsCounts(myTeamID)
     local positions = {}
     local unit_ids = {}
-    local units = Spring.GetTeamUnits(myTeamID)
-    for i = ipairs(units) do
+    local units = SpringGetTeamUnits(myTeamID)
+    for i, unit_id in ipairs(units) do
         local offset = (i - 1) * spacing
         local pos = {
             leader_pos.x + offset * wind_vector.x,
             leader_pos.z + offset * wind_vector.z,
         }
         table.insert(positions, pos)
-        table.insert(unit_ids,  units[i])
+        table.insert(unit_ids,  unit_id)
     end
 
     return {
